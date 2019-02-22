aa5ecb101c2f77eccb87333bb1c5b70a4fe0cb0f
EvanSimon <evansimon07@gmail.com>
2019-02-22 08:49:07 -0500

    i

---
+++ RedditAPI/Models/RedditPost.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RedditAPI.Models
+{
+    public class RedditPost
+    {
+
+        //Model of things I want from the Reddit API
+        public string Title { get; set; }
+        public string ImageURL { get; set; }
+        public string LinkURL { get; set; }
+        
+    }
+}
